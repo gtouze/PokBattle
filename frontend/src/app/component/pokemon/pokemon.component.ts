41a363a9ffcb854f9e80694e021ff0ee083f4f69
Coblestone <antoinedrouard@gmail.com>
2020-05-28 18:52:59 +0200

    Ajout Capacite vue pokemon

--- frontend/src/app/component/pokemon/pokemon.component.ts
+++ frontend/src/app/component/pokemon/pokemon.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { PokemonService } from 'src/app/_webservices/pokemon.webservice';
 import { Pokemon } from 'src/app/models/pokemon.model';
+import { Capacite } from 'src/app/models/capacite.model';
+import { CapaciteService } from 'src/app/_webservices/capacite.webservice';
 
 @Component({
   selector: 'app-pokemon',
@@ -12,15 +14,29 @@ export class PokemonComponent implements OnInit {
   @Input() team: any;
 
   pokemon: Pokemon;
+  capacite1: Capacite;
+  capacite2: Capacite;
 
-  constructor(private pokemonService: PokemonService) { }
+  constructor(private pokemonService: PokemonService, private capaciteService: CapaciteService) { }
 
   ngOnInit(): void {
     this.pokemonService.getPokemonById(this.team.pokemon).subscribe((poke: Pokemon) => {
-      this.pokemon = poke;
+      this.pokemon = poke[0];
     }, (err) => {
       console.error(err);
     });
+    this.capaciteService.getCapaciteById(this.team.capacite1).subscribe((capa: Capacite) => {
+      this.capacite1 = capa[0];
+    }, (err) => {
+      console.error(err);
+    });
+    if (this.team.capacite2) {
+      this.capaciteService.getCapaciteById(this.team.capacite2).subscribe((capa: Capacite) => {
+        this.capacite2 = capa[0];
+      }, (err) => {
+        console.error(err);
+      });
+    }
   }
 
 }
