e2a2b05d16542d5ab8e885ebf61417b0bb5bf903
gurvan <gurvan.t@orange.fr>
2020-06-04 09:00:19 +0200

    page derrière conexion Ok

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -4,16 +4,16 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize(
     "pokbattle",
     "root",
-    "admin",
-  {
-    port: '3306',
-    host: 'localhost',
-    dialect: "mysql",
-    operatorsAliases: false,
-    define: {
-      timestamps: false
+    "",
+    {
+        port: '3308',
+        host: 'localhost',
+        dialect: "mysql",
+        operatorsAliases: false,
+        define: {
+            timestamps: false
+        }
     }
-  }
 );
 
 const db = {};
